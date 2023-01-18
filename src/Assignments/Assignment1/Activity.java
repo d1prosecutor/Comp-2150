a7cf2331251fd18c42a631ed7759da9cc8f53d88
Paradox <paradoxlegendz@gmail.com>
2023-01-18 17:09:38 -0600

    Assignment 1

---
+++ src/Assignments/Assignment1/Activity.java
@@ -0,0 +1,12 @@
+package Assignments.Assignment1;
+
+public abstract class Activity
+{
+    public static int TOTAL_TIME_SPENT_IN_ALL = 0;
+    public int TOTAL_TIME_SPENT;
+
+    public Activity()
+    {
+
+    }
+}
---
+++ src/Assignments/Assignment1/Walking.java
@@ -0,0 +1,5 @@
+package Assignments.Assignment1;
+
+public class Walking
+{
+}
